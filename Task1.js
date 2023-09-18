17609fd34f2485c62208ede3f94d7a82f6d96259
Malosaimi55 <malosaimi379@gmail.com>
2023-09-18 19:55:47 +0300

    done

---
+++ Task1.js
@@ -0,0 +1,115 @@
+/******************************
+	Q1) Create an object called person with properties name, age, and city.
+ Set appropriate values for these properties.
+********************************/
+let person = {
+    name: "Mohammed",
+    age: 18,
+    city: "Kuwait city"
+}
+
+/******************************
+	Q2) Add a new key email to the person object and assign it an email address.
+********************************/
+
+person.email = "malsoaimi379@gmail.com";
+
+/******************************
+	Q3) Write a JavaScript function called hasKey that takes two arguments:
+ an object and a key. The function should return true if the object contains
+ the specified key, and false otherwise. For example, if the object is
+ { name: 'John', age: 25 }, and the key is 'name', the function should return true,
+ but if the key is 'city', it should return false.
+********************************/
+const movies = [
+    {
+      title: "The Shawshank Redemption",
+      director: "Frank Darabont",
+      year: 1994,
+      genre: "Drama",
+    },
+    {
+      title: "The Godfather",
+      director: "Francis Ford Coppola",
+      year: 1972,
+      genre: "Crime",
+    },
+    {
+      title: "Pulp Fiction",
+      director: "Quentin Tarantino",
+      year: 1994,
+      genre: "Crime",
+    },
+    {
+      title: "The Dark Knight",
+      director: "Christopher Nolan",
+      year: 2008,
+      genre: "Action",
+    },
+    {
+      title: "Forrest Gump",
+      director: "Robert Zemeckis",
+      year: 1994,
+      genre: "Drama",
+    },
+];
+
+
+
+function hasKey(obj, key)
+{
+    return key in obj;
+}
+
+console.log(hasKey(person, "nkkjdhdi"));
+
+/******************************
+	Q4) Create a function that accepts an array of objects called 'movies' 
+and iterates through it to log the titles of all the movies.
+********************************/
+
+function printAllTitlesOfMovies(movies, key)
+{
+    for(i = 0; i < movies.length; i++)
+    {
+        if(movies[i][key])
+        {
+            console.log(movies[i][key]);
+        }
+    }
+}
+
+printAllTitlesOfMovies(movies, "title");
+
+/******************************
+	Q5) Create a function that accepts an array of objects called 'movies' 
+and iterates through it to count how many movies were released in the year 1994.
+********************************/
+
+function filterYear1994(x)
+{
+    return x.year == 1994;
+}
+
+let filtered = movies.filter(filterYear1994).length;
+console.log(filtered);
+
+/******************************
+	Q6) Write a function that updates the genre of the movie "The Dark Knight"
+ to "Action/Drama" and returns the modified array of movies.
+********************************/
+
+function updateName(arr)
+{
+    const newArray = arr.map((x) => {
+        if (x.title === "The Dark Knight") {
+            x.genre = "Action/Drama";
+        }
+        return x;
+    });
+
+    return newArray;
+   
+}
+
+console.log(updateName(movies));
